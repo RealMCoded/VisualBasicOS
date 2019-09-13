51caae275eea67f68842af7cb04188345a8b6dba
MCoded <stampylongno1@outlook.com>
2019-09-13 19:25:11 -0400

    Revert "Penguin is now animated and added a new icon."
    This reverts commit b2e8c65ddfcd2b4bba893802be0cca3854a9eae3.

--- VisualBasicOS/secret.vb
+++ VisualBasicOS/secret.vb
@@ -1,7 +1,7 @@
 ﻿Public Class secret
     Private Sub Secret_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' PictureBox1.BackgroundImage = Image.FromFile(".\assets\misc\cp_dance.gif")
-        ' PictureBox1.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox1.BackgroundImage = Image.FromFile(".\assets\misc\cp_dance.gif")
+        PictureBox1.BackgroundImageLayout = ImageLayout.Stretch
         My.Computer.Audio.Play(".\assets\sounds\pizza.wav", AudioPlayMode.BackgroundLoop)
     End Sub
 End Class
