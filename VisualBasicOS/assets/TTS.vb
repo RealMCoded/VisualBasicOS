9e4e329686784706b043b25b2d254894074d6283
MCoded <stampylongno1@outlook.com>
2019-09-28 22:33:34 -0400

    TTS App Now Works.

--- VisualBasicOS/assets/TTS.vb
+++ VisualBasicOS/assets/TTS.vb
@@ -1,3 +1,13 @@
 ﻿Public Class TTS
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim SAPI
+        SAPI = CreateObject("SAPI.spvoice")
+        SAPI.Speak(TextBox1.Text)
+    End Sub
 
+    Private Sub TTS_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim SAPI
+        SAPI = CreateObject("SAPI.spvoice")
+        SAPI.Speak(" ")
+    End Sub
 End Class
