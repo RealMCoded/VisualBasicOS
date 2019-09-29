58e840cbc1af11aa1c5326894ff4011be74e60fe
MCoded <stampylongno1@outlook.com>
2019-09-29 10:15:47 -0400

    Stop Audio button is now in the task bar.

--- VisualBasicOS/OS.Designer.vb
+++ VisualBasicOS/OS.Designer.vb
@@ -68,6 +68,7 @@ Partial Class OS
         Me.Panel1.BackColor = System.Drawing.Color.Black
         Me.Panel1.Controls.Add(Me.WinClock)
         Me.Panel1.Controls.Add(Me.AMB)
+        Me.Panel1.Controls.Add(Me.Button1)
         Me.Panel1.ForeColor = System.Drawing.Color.Black
         Me.Panel1.Location = New System.Drawing.Point(0, 390)
         Me.Panel1.Name = "Panel1"
@@ -79,7 +80,7 @@ Partial Class OS
         Me.WinClock.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.WinClock.AutoSize = True
         Me.WinClock.ForeColor = System.Drawing.Color.White
-        Me.WinClock.Location = New System.Drawing.Point(612, 3)
+        Me.WinClock.Location = New System.Drawing.Point(600, 6)
         Me.WinClock.Name = "WinClock"
         Me.WinClock.Size = New System.Drawing.Size(34, 13)
         Me.WinClock.TabIndex = 1
@@ -109,7 +110,6 @@ Partial Class OS
         Me.Panel2.Controls.Add(Me.Label8)
         Me.Panel2.Controls.Add(Me.PictureBox4)
         Me.Panel2.Controls.Add(Me.Label7)
-        Me.Panel2.Controls.Add(Me.Button1)
         Me.Panel2.Controls.Add(Me.Label6)
         Me.Panel2.Controls.Add(Me.PictureBox2)
         Me.Panel2.Controls.Add(Me.Label5)
@@ -176,7 +176,7 @@ Partial Class OS
         '
         Me.Label7.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(718, 79)
+        Me.Label7.Location = New System.Drawing.Point(720, 44)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(33, 13)
         Me.Label7.TabIndex = 18
@@ -184,12 +184,12 @@ Partial Class OS
         '
         'Button1
         '
-        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
         Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.Button1.Location = New System.Drawing.Point(714, 0)
+        Me.Button1.Location = New System.Drawing.Point(724, 0)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(34, 34)
+        Me.Button1.Size = New System.Drawing.Size(27, 24)
         Me.Button1.TabIndex = 17
         Me.ToolTip1.SetToolTip(Me.Button1, "Stops All Audio That is currently Playing. (In The OS Enviorment.)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10))
         Me.Button1.UseVisualStyleBackColor = True
@@ -219,7 +219,7 @@ Partial Class OS
         '
         Me.Label5.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(714, 61)
+        Me.Label5.Location = New System.Drawing.Point(716, 26)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(34, 13)
         Me.Label5.TabIndex = 14
@@ -250,7 +250,7 @@ Partial Class OS
         '
         Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(718, 45)
+        Me.Label2.Location = New System.Drawing.Point(720, 10)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(28, 13)
         Me.Label2.TabIndex = 11
