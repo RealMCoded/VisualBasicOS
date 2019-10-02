f051a8e368d1db9a15f712953b34b68392a5d46e
MCoded <stampylongno1@outlook.com>
2019-10-02 15:53:27 -0400

    new music folder, moved pizza.wav into it, updated secret.vb

--- VisualBasicOS/secret.vb
+++ VisualBasicOS/secret.vb
@@ -2,7 +2,7 @@
     Private Sub Secret_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' PictureBox1.BackgroundImage = Image.FromFile(".\assets\misc\cp_dance.gif")
         'PictureBox1.BackgroundImageLayout = ImageLayout.Stretch
-        My.Computer.Audio.Play(".\assets\sounds\pizza.wav", AudioPlayMode.BackgroundLoop)
+        My.Computer.Audio.Play(".\assets\sounds\music\pizza.wav", AudioPlayMode.BackgroundLoop)
     End Sub
 
     Private Sub Secret_Leave(sender As Object, e As EventArgs) Handles MyBase.Leave
