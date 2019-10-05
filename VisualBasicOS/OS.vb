cb84fdd9231e094db95a4a0c9224f1830a402330
MCoded <stampylongno1@outlook.com>
2019-10-04 23:02:38 -0400

    "added" file aka Modified File perms

--- VisualBasicOS/OS.vb
+++ VisualBasicOS/OS.vb
@@ -59,6 +59,7 @@
 
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
         about.Show()
+
     End Sub
 
     Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs) Handles PictureBox1.Click
